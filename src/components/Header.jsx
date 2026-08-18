198593fe88fefbf958212fa3b4d191aa15cfc683
melihkostk <melih1kostak@gmail.com>
2026-08-18 11:39:49 +0300

    Language select pop-up added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import logo from "../assets/logo.svg"
 import turkey from "../assets/turkey.svg"
+import english from "../assets/en.svg"
+import russia from "../assets/ru.svg"
+import downArrow from "../assets/down-arrow.png"
 import { Link } from "react-router-dom"
+import React from "react"
 
 export function Header() {
+    const [languageShown , setLanguageShown] = React.useState(false);
+
     return (
         <div className="py-7.5 w-full max-w-350">
-            <div className="flex items-center justify-between"> 
+            <div className="flex items-center justify-between">
                 <div className="w-55 min-w-55">
                     <img className="max-w-full" src={logo} alt="" />
                 </div>
@@ -35,9 +41,26 @@ export function Header() {
                     </ul>
                 </div>
                 <div className="flex items-center justify-between gap-2.5">
-                    <div className="text-[#202529] hover:text-white cursor-pointer flex items-center justify-start gap-2.5 h-12.5 bg-white px-5 hover:bg-[#B7B9BF] transition-colors duration-300 ease-in-out rounded-[5px]">
-                        <img className="w-5 h-5 object-fill rounded-[5px]" src={turkey} alt="" />
-                        <p>Türkçe</p>
+                    <div onMouseLeave={() => setLanguageShown(false)} onMouseEnter={() => setLanguageShown(true)} className="relative">
+                        <div className="text-[#202529] hover:text-white cursor-pointer flex items-center justify-start gap-2.5 h-12.5 bg-white px-5 hover:bg-[#B7B9BF] transition-colors duration-300 ease-in-out rounded-[5px]">
+                            <img className="w-5 h-5 object-fill rounded-[5px]" src={turkey} alt="" />
+                            <p>Türkçe</p>
+                            <img className="w-4 h-4" src={downArrow} alt="" />
+                        </div>
+                        {languageShown && <ul className="bg-white shadow-[0_0_30px_rgba(0,0,0,0.1)] rounded-[10px] p-5 w-40 min-w-full absolute">
+                            <li className="flex items-center gap-2.5 pb-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                                <img className="w-5 h-5 object-fill rounded-[5px]" src={turkey} alt="" />
+                                <p className="font-semibold text-sm">Türkçe</p>
+                            </li>
+                            <li className="flex items-center gap-2.5 py-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                                <img className="w-5 h-5 object-fill rounded-[5px]" src={english} alt="" />
+                                <p className="font-semibold text-sm">English</p>
+                            </li>
+                            <li className="flex items-center gap-2.5 pt-2 hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                                <img className="w-5 h-5 object-fill rounded-[5px]" src={russia} alt="" />
+                                <p className="font-semibold text-sm">Русский</p>
+                            </li>
+                        </ul>}
                     </div>
                     <div>
                         <button className="bg-[#27C5D2] cursor-pointer px-5 h-12.5 rounded-[5px] text-white font-semibold hover:bg-[#048B99] transition-colors duration-300 ease-in-out">Giriş Yap</button>
