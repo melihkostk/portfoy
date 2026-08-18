c1f6cfae303b4f7de25fb4119a1f6e4e05c66311
melihkostk <melih1kostak@gmail.com>
2026-08-18 12:42:24 +0300

    Application status inquiry pop-up added to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,13 @@ import whiteDownArrow from "../assets/white-down-arrow.png"
 import { Link } from "react-router-dom"
 import person from "../assets/person.png"
 import arrowRight from "../assets/arrow-right.png"
+import plus from "../assets/plus.png"
 import React from "react"
 
 export function Header() {
     const [languageShown, setLanguageShown] = React.useState(false);
     const [registerShown, setRegisterShown] = React.useState(false);
+    const [applicationShown , setApplicationShown] = React.useState(false);
 
     return (
         <div className="py-7.5 w-full max-w-350">
@@ -45,22 +47,22 @@ export function Header() {
                     </ul>
                 </div>
                 <div className="flex items-center justify-between gap-2.5">
-                    <div onMouseLeave={() => setLanguageShown(false)} onMouseEnter={() => setLanguageShown(true)} className="relative">
-                        <div className="text-[#202529] hover:text-white cursor-pointer flex items-center justify-start gap-2.5 h-12.5 bg-white px-5 hover:bg-[#B7B9BF] transition-colors duration-300 ease-in-out rounded-[5px]">
+                    <div onMouseLeave={() => setLanguageShown(false)} onMouseEnter={() => setLanguageShown(true)} className="rounded-[5px] relative hover:bg-[#B7B9BF] hover:text-white text-[#202529] transition-colors duration-300 ease-in-out">
+                        <div className="cursor-pointer flex items-center justify-start gap-2.5 h-12.5 px-5">
                             <img className="w-5 h-5 object-fill rounded-[5px]" src={turkey} alt="" />
-                            <p>Türkçe</p>
+                            <p className="font-semibold">Türkçe</p>
                             <img className="w-4 h-4" src={downArrow} alt="" />
                         </div>
                         {languageShown && <ul className="bg-white z-50 shadow-[0_0_30px_rgba(0,0,0,0.1)] rounded-[10px] p-5 w-40 min-w-full absolute">
-                            <li className="flex items-center gap-2.5 pb-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                            <li className="text-[#212529] flex items-center gap-2.5 pb-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
                                 <img className="w-5 h-5 object-fill rounded-[5px]" src={turkey} alt="" />
                                 <p className="font-semibold text-sm">Türkçe</p>
                             </li>
-                            <li className="flex items-center gap-2.5 py-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                            <li className=" text-[#212529] flex items-center gap-2.5 py-2 border-b border-b-[#E5E5EA] hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
                                 <img className="w-5 h-5 object-fill rounded-[5px]" src={english} alt="" />
                                 <p className="font-semibold text-sm">English</p>
                             </li>
-                            <li className="flex items-center gap-2.5 pt-2 hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
+                            <li className=" text-[#212529] flex items-center gap-2.5 pt-2 hover:text-[#27C5D2] transition-colors duration-300 ease-in-out cursor-pointer">
                                 <img className="w-5 h-5 object-fill rounded-[5px]" src={russia} alt="" />
                                 <p className="font-semibold text-sm">Русский</p>
                             </li>
@@ -81,8 +83,20 @@ export function Header() {
                             <a className="w-full" href="">Kayıt Ol</a>
                         </div>}
                     </div>
-                    <div>
-                        <button className="bg-[#048B99] cursor-pointer px-5 h-12.5 rounded-[5px] text-white font-semibold hover:bg-[#026872] transition-colors duration-300 ease-in-out">Firma başvurusu</button>
+                    <div className="relative">
+                        <div className="bg-[#048B99] cursor-pointer flex items-center h-12.5 rounded-[5px] text-white font-semibold">
+                            <div className="flex items-center px-5 h-full rounded-l-[5px] border-r border-r-[#ffffff38] gap-1 hover:bg-[#026872] transition-colors duration-300 ease-in-out">
+                                <img className="w-5 h-5" src={plus} alt="" />
+                                <p>Firma Başvurusu</p>
+                            </div>
+                            <div onClick={() => setApplicationShown(prev => !prev)} className="flex h-full rounded-r-[5px] px-1.5 items-center justify-center hover:bg-[#026872] hover:shadow-[0_0_30px_#026872] transition-[background-color,box-shadow] duration-300 ease-in-out">
+                                <img className="w-4.5 h-4.5" src={whiteDownArrow} alt="" />
+                            </div>
+                        </div>
+                        {applicationShown && <div className="absolute w-full bg-[#048B99] text-sm font-semibold cursor-pointer max-w-full text-white py-2 px-2.5 mt-0.5 rounded-[5px] flex items-center gap-1 hover:pl-4 transition-all duration-300 ease-in-out">
+                            <img className="w-2.5 h-2.5" src={arrowRight} alt="" />
+                            <a href="">Başvuru Sorgulama</a>
+                        </div>}
                     </div>
                 </div>
             </div>
