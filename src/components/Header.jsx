1eccc5a3725f44279b2f2c61835eae3969db8339
melihkostk <melih1kostak@gmail.com>
2026-08-20 16:12:07 +0300

    Account transactions pop-up added to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,8 @@ export function Header() {
     const [registerShown, setRegisterShown] = React.useState(false);
     const [applicationShown, setApplicationShown] = React.useState(false);
     const [loged] = React.useState(true)
-    const[notShown, setNotShown] = React.useState(false)
+    const [notShown, setNotShown] = React.useState(false)
+    const [accountShown, setAccountShown] = React.useState(false)
 
     return (
         <div className="py-7.5 w-full max-w-350">
@@ -105,7 +106,7 @@ export function Header() {
                         <button onClick={() => setNotShown(prev => !prev)} className="w-12.5 h-12.5 bg-[#f8f8f8] rounded-lg flex items-center justify-center cursor-pointer hover:bg-[#27C5D2] transition-colors duration-300 ease-in-out">
                             <img className="w-6 h-6" src={notification} alt="" />
                         </button>
-                        {notShown && <div className="absolute w-75 rounded-lg bg-white right-0 shadow-[0_0_30px_rgba(0,0,0,0.1)] py-3.75">
+                        {notShown && <div className="absolute z-50 w-75 rounded-lg bg-white right-0 shadow-[0_0_30px_rgba(0,0,0,0.1)] py-3.75">
                             <div className="bg-[#27C5D2] p-7.5 rounded-t-lg">
                                 <p className="text-white text-sm mb-5">
                                     Okunmamış 129 Bildirim
@@ -140,16 +141,54 @@ export function Header() {
                         <img className="object-cover w-5 h-5" src={shop} alt="" />
                         <span>Portföyüm</span>
                     </Link>}
-                    {loged && <div className="flex">
-                        <div className="h-12.5 border-r border-r-[#ffffff38]">
-                            <Link className="flex items-center h-full px-5 rounded-l-[5px] bg-[#27C5D2] cursor-pointer hover:bg-[#048B99] transition-colors duration-300 ease-in-out">
-                                <img className="object-cover w-7 h-7" src={person} alt="" />
-                                <span className="text-white">Hesabım</span>
-                            </Link>
+                    {loged && <div className="relative">
+                        <div className="flex">
+                            <div className="h-12.5 border-r border-r-[#ffffff38]">
+                                <Link className="flex items-center h-full px-5 rounded-l-[5px] bg-[#27C5D2] cursor-pointer hover:bg-[#048B99] transition-colors duration-300 ease-in-out">
+                                    <img className="object-cover w-7 h-7" src={person} alt="" />
+                                    <span className="text-white">Hesabım</span>
+                                </Link>
+                            </div>
+                            <div onClick={() => setAccountShown(prev => !prev)} className="flex items-center justify-center bg-[#27C5D2] rounded-r-[5px] w-7.5 cursor-pointer hover:bg-[#026872] hover:shadow-[0_0_30px_#026872] transition-[background-color,box-shadow] duration-300 ease-in-out">
+                                <img className=" w-4 h-4" src={whiteDownArrow} alt="" />
+                            </div>
                         </div>
-                        <div className="flex items-center justify-center bg-[#27C5D2] rounded-r-[5px] w-7.5 cursor-pointer hover:bg-[#026872] hover:shadow-[0_0_30px_#026872] transition-[background-color,box-shadow] duration-300 ease-in-out">
-                            <img className=" w-4 h-4" src={whiteDownArrow} alt="" />
-                        </div>
+                        {accountShown && <div className="bg-[#27C5D2] absolute w-full top-full mt-0.5 rounded-[5px] px-2.5">
+                            <div className="mt-2.5 pb-1.25 border-b border-b-[#FFFFFF80]">
+                                <p className="text-white text-sm font-semibold">Pigasoft</p>
+                                <p className="text-white text-xs font-medium">Sistem Admin</p>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li className="flex items-center py-2 whitespace-nowrap cursor-pointer overflow-hidden text-ellipsis hover:pl-2 transition-normal duration-300 ease-in-out">
+                                        <img className="w-3 h-3 mr-1.25" src={arrowRight} alt="" />
+                                        <span className="text-white text-sm">Favorilerim</span>
+                                    </li>
+                                    <li className="flex items-center py-2 whitespace-nowrap cursor-pointer overflow-hidden text-ellipsis hover:pl-2 transition-normal duration-300 ease-in-out">
+                                        <img className="w-3 h-3 mr-1.25" src={arrowRight} alt="" />
+                                        <span className="text-white text-sm truncate">
+                                            Müşteriye gönderilen teklifler
+                                        </span>
+                                    </li>
+                                    <li className="flex items-center py-2 whitespace-nowrap cursor-pointer overflow-hidden text-ellipsis hover:pl-2 transition-normal duration-300 ease-in-out">
+                                        <img className="w-3 h-3 mr-1.25" src={arrowRight} alt="" />
+                                        <span className="text-white text-sm">
+                                            Yardım & Destek
+                                        </span>
+                                    </li>
+                                    <li className="flex items-center py-2 whitespace-nowrap cursor-pointer overflow-hidden text-ellipsis hover:pl-2 transition-normal duration-300 ease-in-out">
+                                        <img className="w-3 h-3 mr-1.25" src={arrowRight} alt="" />
+                                        <span className="text-white text-sm">Yönetim Paneli</span>
+                                    </li>
+                                    <li className="flex items-center py-2 whitespace-nowrap cursor-pointer overflow-hidden text-ellipsis hover:pl-2 transition-normal duration-300 ease-in-out">
+                                        <img className="w-3 h-3 mr-1.25" src={arrowRight} alt="" />
+                                        <span className="text-white text-sm">
+                                            Çıkış Yap
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>}
                     </div>}
                 </div>
             </div>
