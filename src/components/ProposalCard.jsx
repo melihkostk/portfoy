305668fc3e27bd34951bdad7b1c7669b9481a5f6
melihkostk <melih1kostak@gmail.com>
2026-08-26 17:12:53 +0300

    Added breakpoints to the proposals page

--- src/components/ProposalCard.jsx
+++ src/components/ProposalCard.jsx
@@ -3,11 +3,11 @@ export function ProposalCard() {
         <tr>
             <td className="py-3 px-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-[30ch] truncate">11052274242249846424</td>
             <td className="py-3 px-2">hhhhyy</td>
-            <td className="py-3 px-2 text-[#6c757d] text-sm">Henüz değerlendirilmedi</td>
+            <td className="py-3 px-2 text-[#6c757d] text-sm whitespace-nowrap">Henüz değerlendirilmedi</td>
             <td className="py-3 px-2">
                 <span className="bg-[#27C5D2] text-xs text-white font-semibold py-1.25 px-2 rounded-md">Beklemede</span>
             </td>
-            <td className="py-3 px-2">19 Ocak 2026</td>
+            <td className="py-3 px-2 whitespace-nowrap">19 Ocak 2026</td>
             <td className="text-end py-3 px-2">
                 <button className="bg-[#27C5D2] text-xs text-white font-semibold py-2 px-5 rounded-sm cursor-pointer hover:bg-[#026872] transition-colors duration-300 ease-in-out">Görüntüle</button>
             </td>
--- src/pages/Proposals.jsx
+++ src/pages/Proposals.jsx
@@ -9,27 +9,27 @@ export function Proposals() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%]">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full">
                         <Sidebar />
                     </div>
-                    <div className="w-[72%] pl-7.5">
+                    <div className="w-[72%]  max-[992px]:w-full pl-7.5  max-[992px]:pl-0">
                         <h2 className="text-[32px] text-[#212529] mb-2">Müşteriye gönderilen teklifler</h2>
-                        <div>
+                        <div className="overflow-auto">
                             <table className="w-full">
                                 <thead>
                                     <tr>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg">#</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Müşteri</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Değerlendirme</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Durum</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Oluşturma Tarihi</th>
-                                        <th className="text-end bg-[#ececec] py-4 px-2.5 rounded-r-lg">İşlemler</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap rounded-l-lg">#</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Müşteri</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Değerlendirme</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Durum</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Oluşturma Tarihi</th>
+                                        <th className="text-end bg-[#ececec] py-4 px-2.5 whitespace-nowrap rounded-r-lg">İşlemler</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -51,8 +51,8 @@ export function Proposals() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-30 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-30 mb-30 max-[992px]:mt-7.5 max-[992px]:mb-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
