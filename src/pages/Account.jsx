4fb66888426911f12e493d94f29666385a269b5e
melihkostk <melih1kostak@gmail.com>
2026-08-26 16:50:55 +0300

    Added breakpoints to the account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,25 +3,26 @@ import { Sidebar } from "../components/Sidebar"
 import pg from "../assets/pg.jpg"
 import { AppLinks } from "../components/AppLinks"
 import { Footer } from "../components/Footer"
+import { Link } from "react-router-dom"
 
 export function Account() {
     return (
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%]">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full">
                         <Sidebar />
                     </div>
-                    <div className="pl-7.5 w-[72%]">
-                        <h2 className="text-[#212529] text-[25px] font-semibold">Firma Bilgilerim</h2>
-                        <div className="flex p-7.5 border border-[#eee] rounded-lg mb-10">
-                            <div className="flex w-1/2">
+                    <div className="pl-7.5 w-[72%] max-[992px]:w-full max-[992px]:pl-0">
+                        <h2 className="text-[#212529] text-[25px] mb-5 font-semibold">Firma Bilgilerim</h2>
+                        <div className="flex p-7.5 border border-[#eee] rounded-lg mb-10 max-[992px]:flex-col max-[992px]:gap-5">
+                            <div className="flex w-1/2 max-[992px]:flex-col max-[992px]:w-full">
                                 <div className="w-20 h-20 mr-5">
                                     <img className="max-w-full align-middle" src={pg} alt="" />
                                 </div>
@@ -42,22 +43,22 @@ export function Account() {
                                         </li>
                                     </ul>
                                     <div className="mt-7.5">
-                                        <a className="text-xs text-[#909090] font-semibold hover:text-black transition-colors duration-300 ease-in-out" href="">
+                                        <Link to={"/company"} className="text-xs text-[#909090] font-semibold hover:text-black transition-colors duration-300 ease-in-out" href="">
                                             Firma Profilini Görüntüle
-                                        </a>
+                                        </Link>
                                     </div>
 
                                 </div>
                             </div>
-                            <div className="flex items-center w-1/2">
+                            <div className="flex items-center w-1/2 max-[992px]:w-full">
                                 <div>
                                     <h3 className="text-xl text-[#212529] mb-2 font-medium">Abonelik Bilgileri</h3>
                                     <div>
-                                        <div className="flex items-center gap-1.25">
-                                            <p className="text-[25px] text-[#212529] font-semibold w-[80%]">
+                                        <div className="flex items-center max-[992px]:flex-col max-[992px]:items-start gap-1.25">
+                                            <p className="text-[25px] max-[992px]:text-lg text-[#212529] font-semibold w-[80%] max-[992px]:w-full">
                                                 Premium Pazarlama Firması Paket
                                             </p>
-                                            <p className="w-[20%] bg-[#f8f8f8] py-1.25 px-2 rounded-lg text-sm text-center">
+                                            <p className="w-[20%] bg-[#f8f8f8] py-1.25 px-2 rounded-lg text-sm text-center max-[992px]:max-w-fit max-[992px]:whitespace-nowrap">
                                                 Deneme Sürümü
                                             </p>
                                         </div>
@@ -83,7 +84,7 @@ export function Account() {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="w-1/2 pr-3">
+                                    <div className="w-1/2 pr-3 max-[992px]:w-full max-[992px]:mb-2.5 max-[992px]:pr-0">
                                         <label htmlFor="">Telefon</label>
                                         <div className="flex items-center gap-3.75">
                                             <div>
@@ -103,7 +104,7 @@ export function Account() {
                                 </div>
                             </form>
                         </div>
-                        <div>
+                        <div className="max-[992px]:mb-10">
                             <h2 className="text-[25px] mb-5 font-semibold">Şifre Değiştir</h2>
                             <div className="mb-4 bg-[#fff3cd] text-[#664d03] p-4 rounded-lg">
                                 Henüz şifre değişikliği yok
@@ -113,8 +114,8 @@ export function Account() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-30 mb-10'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-30 mb-10 max-[992px]:mb-7.5 max-[992px]:mt-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
