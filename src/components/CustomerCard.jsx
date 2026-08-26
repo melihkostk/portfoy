4e7526afb3e965a84399d9a76fa43b0ebe53d254
melihkostk <melih1kostak@gmail.com>
2026-08-26 14:11:38 +0300

    Added breakpoints to the customers page

--- src/components/CustomerCard.jsx
+++ src/components/CustomerCard.jsx
@@ -4,11 +4,11 @@ export function CustomerCard(){
     return(
         <tr>
             <td className="py-3 px-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-[30ch] truncate">Adı Adı</td>
-            <td className="py-3 px-2 text-[#212529] text-base">adı@gmail.com</td>
-            <td className="py-3 px-2 text-[#212529] text-base">
+            <td className="py-3 px-2 text-[#212529] text-base whitespace-nowrap">adı@gmail.com</td>
+            <td className="py-3 px-2 text-[#212529] text-base whitespace-nowrap">
                 90 44445555333
             </td>
-            <td className="py-3 px-2 text-[#212529] text-base">
+            <td className="py-3 px-2 text-[#212529] text-base whitespace-nowrap">
                 Türkçe
             </td>
             <td>
@@ -19,9 +19,9 @@ export function CustomerCard(){
            </td>
            <td className="flex items-center gap-1 py-3 px-2">
                 <button className="bg-[#f1f1f1] py-2 px-5 rounded-lg cursor-pointer hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out">
-                    <img className="w-5 h-5" src={pencil} alt="" />
+                    <img className="w-5 h-5 min-w-5 min-h-5" src={pencil} alt="" />
                 </button>
-                <div className="bg-[#f1f1f1] py-2 px-5 rounded-lg text-sm text-[#4b4b4b] font-semibold cursor-pointer hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out">
+                <div className="bg-[#f1f1f1] py-2 px-5 rounded-lg whitespace-nowrap text-sm text-[#4b4b4b] font-semibold cursor-pointer hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out">
                     Hiç teklif yok
                 </div>
            </td>
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -10,33 +10,33 @@ export function Customers() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Portföyüm</span></p>
                 </div>
             </div>
             <CompanyHeader />
-            <div className="w-full max-w-350 mt-12.5">
-                <div className="flex justify-between items-center mb-5">
+            <div className="w-full max-w-[90%] mt-12.5">
+                <div className="flex justify-between items-center mb-5 flex-wrap">
                     <h2 className="text-[#212529] text-[32px]">Müşteriler</h2>
                     <div className="flex items-center gap-2.5">
-                        <div className="flex items-center relative">
+                        <div className="flex items-center relative max-[992px]:w-full">
                             <img className="w-4 h-4 absolute left-1" src={graySearch} alt="" />
                             <input className="text-sm pl-7 rounded-lg h-9.25 placeholder:text-sm focus:outline-none focus:ring-0 focus:bg-[#f8f8f8]" type="text" placeholder="Müşteri adı ile arayın" />
                         </div>
-                        <a className="text-[#4b4b4b] bg-[#f1f1f1] text-sm py-2 px-5 font-semibold rounded-lg hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out cursor-pointer" href="">Müşteri Oluştur</a>
+                        <a className="text-[#4b4b4b] bg-[#f1f1f1] whitespace-nowrap text-sm py-2 px-5 font-semibold rounded-lg hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out cursor-pointer" href="">Müşteri Oluştur</a>
                     </div>
                 </div>
-                <div>
+                <div className="overflow-auto">
                     <table className="w-full">
                         <thead>
                             <tr>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg">İsim ve Soyisim</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5">E-Posta</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5">Telefon</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5">Dil</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5">Notunuz</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5">Oluşturma Tarihi</th>
-                                <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-r-lg">Teklif ve İşlemler</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg whitespace-nowrap">İsim ve Soyisim</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">E-Posta</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Telefon</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Dil</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Notunuz</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Oluşturma Tarihi</th>
+                                <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-r-lg whitespace-nowrap">Teklif ve İşlemler</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -51,7 +51,7 @@ export function Customers() {
                 </div>
             </div>
             <div className='w-full mt-40 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
