155641e1a859248f2b7b22c62d12ea4b667c312d
melihkostk <melih1kostak@gmail.com>
2026-08-26 17:23:16 +0300

    Added breakpoints to the wishlist page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -9,31 +9,31 @@ export function Offers() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%] sticky top-0">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full sticky top-0">
                         <Sidebar />
                     </div>
-                    <div className="w-[72%] pl-7.5">
+                    <div className="w-[72%] max-[992px]:w-full pl-7.5 max-[992px]:pl-0">
                         <h2 className="text-[32px] text-[#212529] font-medium mb-2">Aldığım teklifler</h2>
                         <div className="p-4 mb-4 bg-[#fff3cd] rounded-lg">
                             <p className="text-[#664d03]">Bu sayfada sadece sizin oluşturduğunuz ilanların tekliflerini görüntüleyebilirsiniz.</p>
                         </div>
-                        <div>
+                        <div className="overflow-auto">
                             <table className="w-full">
                                 <thead>
                                     <tr>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg">İlan</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Geçerli Fiyat</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Pass / Komisyon</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5 ">Teklif Edilen Tutar</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Durum</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Oluşturma Tarihi</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-r-lg">İşlemler</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg whitespace-nowrap">İlan</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Geçerli Fiyat</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Pass / Komisyon</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap ">Teklif Edilen Tutar</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Durum</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Oluşturma Tarihi</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap rounded-r-lg">İşlemler</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -59,8 +59,8 @@ export function Offers() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-40 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-40 mb-30 max-[992px]:mt-7.5 max-[992px]:mb-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -10,17 +10,17 @@ export function Wishlist() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%] sticky top-0">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full sticky top-0">
                         <Sidebar />
                     </div>
-                    <div className="w-[72%] pl-7.5">
-                        <div className="flex justify-between items-center mb-5">
+                    <div className="w-[72%] max-[992px]:w-full pl-7.5 max-[992px]:pl-0">
+                        <div className="flex justify-between items-center mb-5 flex-wrap">
                             <h2 className="text-[32px] text-[#212529] font-medium mb-2">Favorilerim</h2>
                             <select className="border border-[#D9D9D9] rounded-lg py-1.5 px-3 text-[#212529]" name="" id="">
                                 <option value="" selected>
@@ -56,8 +56,8 @@ export function Wishlist() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-40 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-40 mb-30 max-[992px]:mt-7.5 max-[992px]:mb-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
