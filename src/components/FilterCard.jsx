667477fa6126e56150a780cb00fcd372d1c59cb8
melihkostk <melih1kostak@gmail.com>
2026-08-26 17:28:34 +0300

    Added breakpoints to the quick filters page

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -10,12 +10,12 @@ export function FilterCard() {
             </td>
             <td className="py-3 px-2 whitespace-nowrap">08 Mayıs 2025</td>
             <td className="py-3 px-2 flex items-center justify-end gap-2">
-                <a className="text-xs text-white font-semibold bg-[#27C5D2] py-2 px-5 rounded-sm hover:bg-[#026872] transition-colors duration-300 ease-in-out" href="">Seçenekleri Uygula</a>
-                <a className="bg-[#f1f1f1] py-2 px-5 rounded-sm hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out" href="">
-                    <img className="w-4 h-4" src={pencil} alt="" />
+                <a className="text-xs whitespace-nowrap text-white font-semibold bg-[#27C5D2] py-2 px-5 rounded-sm hover:bg-[#026872] transition-colors duration-300 ease-in-out" href="">Seçenekleri Uygula</a>
+                <a className="bg-[#f1f1f1] whitespace-nowrap py-2 px-5 rounded-sm hover:bg-[#c3c3c3] transition-colors duration-300 ease-in-out" href="">
+                    <img className="w-4 h-4 min-h-4 min-w-4" src={pencil} alt="" />
                 </a>
-                <a className="bg-[#dc3545] py-2 px-5 rounded-sm hover:bg-[#bb2d3b] transition-colors duration-300 ease-in-out" href="">
-                    <img className="w-4 h-4" src={bin} alt="" />
+                <a className="bg-[#dc3545] whitespace-nowrap py-2 px-5 rounded-sm hover:bg-[#bb2d3b] transition-colors duration-300 ease-in-out" href="">
+                    <img className="w-4 h-4 min-h-4 min-w-4" src={bin} alt="" />
                 </a>
             </td>
         </tr>
--- src/pages/QuickFilters.jsx
+++ src/pages/QuickFilters.jsx
@@ -9,25 +9,25 @@ export function QuickFilters() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%] sticky top-0">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full sticky top-0">
                         <Sidebar />
                     </div>
-                    <div className="w-[72%] pl-7.5">
+                    <div className="w-[72%] max-[992px]:w-full pl-7.5 max-[992px]:pl-0">
                         <h2 className="text-[32px] text-[#212529] font-medium mb-2">Hızlı Filtreler</h2>
-                        <div>
+                        <div className="overflow-auto">
                             <table className="w-full">
                                 <thead>
                                     <tr>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg">Başlık</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Bildirim</th>
-                                        <th className="text-start bg-[#ececec] py-4 px-2.5">Oluşturma Tarihi</th>
-                                        <th className="text-end bg-[#ececec] py-4 px-2.5 rounded-r-lg">İşlemler</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 rounded-l-lg whitespace-nowrap">Başlık</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Bildirim</th>
+                                        <th className="text-start bg-[#ececec] py-4 px-2.5 whitespace-nowrap">Oluşturma Tarihi</th>
+                                        <th className="text-end bg-[#ececec] py-4 px-2.5 rounded-r-lg whitespace-nowrap">İşlemler</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -49,8 +49,8 @@ export function QuickFilters() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-40 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-40 mb-30 max-[992px]:mt-7.5 max-[992px]:mb-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
