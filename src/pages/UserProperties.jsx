882edbb1c96b75fdb9a05110faccbf1d58246106
melihkostk <melih1kostak@gmail.com>
2026-08-26 16:56:33 +0300

    Added breakpoints to the user properties page

--- src/pages/UserProperties.jsx
+++ src/pages/UserProperties.jsx
@@ -9,16 +9,16 @@ export function UserProperties() {
         <div className='flex flex-col items-center font-sf'>
             <Header />
             <div className="w-full bg-[#f8f8f8] flex justify-center py-2.5 mb-4">
-                <div className="w-full max-w-350">
+                <div className="w-full max-w-[90%]">
                     <p className="text-sm text-[#636363] font-medium">Anasayfa {">"} <span className="text-[#9a9898]"> Hesabım</span></p>
                 </div>
             </div>
-            <div className="w-full max-w-350">
-                <div className="flex items-start">
-                    <div className="w-[28%] sticky top-0">
+            <div className="w-full max-w-[90%]">
+                <div className="flex items-start max-[992px]:flex-col-reverse">
+                    <div className="w-[28%] max-[992px]:w-full sticky top-0">
                         <Sidebar />
                     </div>
-                    <div className="w-[72%] pl-7.5">
+                    <div className="w-[72%] max-[992px]:w-full pl-7.5 max-[992px]:pl-0">
                         <h2 className="text-[32px] text-[#212529] font-medium mb-2">İlanlarım</h2>
                         <div>
                             <PropertiesCard page="myProperties" />
@@ -58,8 +58,8 @@ export function UserProperties() {
                     </div>
                 </div>
             </div>
-            <div className='w-full mt-40 mb-30'>
-                <div className='w-full mx-auto max-w-350 flex flex-col items-center justify-center bg-[#f7f6fb]'>
+            <div className='w-full mt-40 mb-30 max-[992px]:mt-7.5 max-[992px]:mb-7.5'>
+                <div className='w-full mx-auto max-w-[90%] flex flex-col items-center justify-center bg-[#f7f6fb]'>
                     <AppLinks />
                 </div>
             </div>
